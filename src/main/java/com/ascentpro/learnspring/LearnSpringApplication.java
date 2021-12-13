aa6f46c0e81723e0ee6957985977e4d291ac757a
Dell <Dell@DESKTOP-EP6LLFU>
2021-12-13 17:49:08 +0530

    3rd

--- src/main/java/com/ascentpro/learnspring/LearnSpringApplication.java
+++ src/main/java/com/ascentpro/learnspring/LearnSpringApplication.java
@@ -9,6 +9,7 @@ public class LearnSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringApplication.class, args);
 		System.out.println("hi");
+		System.out.println("hi");
 	}
 
 }
